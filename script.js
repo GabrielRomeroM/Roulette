b9f57cb6bca295afb238934472bc2d2b67f7a49f
GabrielRomeroM <gabrielromeromaidana@gmail.com>
2024-09-08 13:39:45 -0300

    V0.1

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-document.getElementById('tirar').onclick=tirarRuleta;
+document.getElementById('tirar').onclick = tirarRuleta;
 var saldo = 100;
 var numeroApuesta = 1;
 var numeroRuleta = -1;
 
 function spinRuleta(){
-  return Math.floor(Math.random() *max)
+  return Math.floor(Math.random() *10)
 }
 
 function tirarRuleta(){
-   numeroApuesta = document.getElementById('numero').value;
-  if(numeroApuesta >=10 || numeroApuesta <0 || numeroApuesta == " "){
-    alert('numero invalido');
+   numeroApuesta = parseFloat(document.getElementById('numero').value);
+
+  if(isNaN(numeroApuesta) || numeroApuesta <0 || numeroApuesta >9 ){
+    alert('Numero invalido, por favor ingrese un numero entre 0 y 9');
     return;
   }
+  
   numeroRuleta = spinRuleta();
-  document.getElementById('ruleta').innerHTML=saldo;
-  if (numeroRuleta == numeroApuesta){
-    alert('GANASTE');
+  document.getElementById('ruleta').innerHTML = numeroRuleta;
+
+  if (numeroRuleta === numeroApuesta){
+    alert('¡GANASTE!');
     aumentarsaldo();
-  }
-  else{
+  }else{
     alert('Segui participando');
-    disminuyesaldo();
+    disminuirSaldo();
   }
 }
 
